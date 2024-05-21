88bb1cc66ee99d78098520da16953c8896b5b746
Luis Pacheco <a640335a@nzxpap1175.nndc.kp.org>
2024-05-20 17:04:25 -0700

    Completes functin to check completes rows from multiple csv files

--- r_programming_w2_pa1.R
+++ r_programming_w2_pa1.R
@@ -38,3 +38,38 @@ pollutantmean <- function (directory, pollutant, id = 1:332) {
 }
 
 pollutantmean ("specdata","nitrate",23)
+
+complete <- function (directory, id = 1:332) {
+    ## 'directory' is a character vector of length 1 indicating the location of the csv file
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+    
+    ## Return a data frame in the form:
+    ## id nobs
+    ## 1  117
+    ## 2  1041
+    ## ...
+    ## where 'id' is the monitor ID number and 'nobs' is the
+    ## number of complete cases
+    
+    library (stringr)
+    
+    completes <- data.frame(matrix (ncol = 2, nrow = 0))
+    completes_colname <- c("id","nobs")
+    colnames(completes) <- completes_colname
+    
+    for (i in id) {
+        pth <- paste(sep = "", getwd(),"/",directory,"/",str_pad(i,3,pad = "0"),".csv")
+        mntr_file <- read.csv(pth)
+        cmplts <- complete.cases(mntr_file)
+        cmplts_len <- sum(cmplts)
+        
+        cmplts_add <- c(id, cmplts_len)
+        completes <- rbind (completes, cmplts_add)
+    }
+    
+    return (completes)
+    
+}
+
+complete ("specdata", 30:26)
